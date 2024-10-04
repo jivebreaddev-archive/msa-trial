f17d74e0bd6c998500667bdb8d1a41cc7fb8f0bc
Jivebread <spark@troy.edu>
2024-10-05 00:07:18 +0900

    initialize

---
+++ cloud-example/src/test/java/com/teamsimon/cloud_example/CloudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsimon.cloud_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
